bc1c471757c03f849b03d2c34d9f4322d05affe4
Scott Guan <yguan44@gatech.edu>
2021-02-28 10:18:08 -0500

    focal_loss implemented.

--- losses/focal_loss.py
+++ losses/focal_loss.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+
 def reweight(cls_num_list, beta=0.9999):
     '''
     Implement reweighting by effective numbers
@@ -14,7 +15,11 @@ def reweight(cls_num_list, beta=0.9999):
     #############################################################################
     # TODO: reweight each class by effective numbers                            #
     #############################################################################
-    
+    E = (1 - np.power(beta, cls_num_list)) / (1 - beta)
+    alpha = 1 / E
+    C = len(cls_num_list)
+    per_cls_weights = alpha / (np.sum(alpha) / C)
+
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -39,7 +44,32 @@ class FocalLoss(nn.Module):
         #############################################################################
         # TODO: Implement forward pass of the focal loss                            #
         #############################################################################
-        
+        n_classes = len(self.weight)
+        target_one_hot = F.one_hot(target, n_classes).float()
+
+        weights = self.weight
+        weights = torch.tensor(weights).float()
+        weights = weights.unsqueeze(0)
+        weights = weights.repeat(target_one_hot.shape[0], 1) * target_one_hot
+        weights = weights.sum(1)
+        weights = weights.unsqueeze(1)
+        weights = weights.repeat(1, n_classes)
+
+        BCLoss = F.binary_cross_entropy_with_logits(input=input, target=target_one_hot, reduction="none")
+
+        if self.gamma == 0.0:
+            modulator = 1.0
+        else:
+            modulator = torch.exp(-self.gamma * target_one_hot * input - self.gamma * torch.log(1 +
+                                                                               torch.exp(-1.0 * input)))
+
+        loss = modulator * BCLoss
+
+        weighted_loss = weights * loss
+
+        focal_loss = torch.sum(weighted_loss)
+        loss = focal_loss / torch.sum(target_one_hot)
+
         #############################################################################
         #                              END OF YOUR CODE                             #
         #############################################################################
